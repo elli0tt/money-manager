aa7b9702cf2f82907175258042cc18be317067dd
Ilya Pestrikov <ilya.pestrikov2@gmail.com>
2021-01-07 14:12:51 +0300

    Added AddTransactionBottomSheetFragment layout, changed bottom sheet dialog with corners behaviour to work with keyboard

--- moneymanager/app/src/main/java/com/elli0tt/money_manager/base/fragment/BaseBottomSheetDialogFragment.kt
+++ moneymanager/app/src/main/java/com/elli0tt/money_manager/base/fragment/BaseBottomSheetDialogFragment.kt
@@ -1,11 +1,52 @@
 package com.elli0tt.money_manager.base.fragment
 
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.DialogFragment
 import com.elli0tt.money_manager.App
+import com.elli0tt.money_manager.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-abstract class BaseBottomSheetDialogFragment : BottomSheetDialogFragment() {
+abstract class BaseBottomSheetDialogFragment(@LayoutRes private val layoutResId: Int) :
+    BottomSheetDialogFragment() {
 
     protected val app get() = context?.applicationContext as App
 
     protected val appComponent get() = app.appComponent
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.BottomSheetDialog)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        dialog.setOnShowListener { dialog ->
+            val d = dialog as BottomSheetDialog
+            val bottomSheet =
+                d.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout?
+            BottomSheetBehavior.from<FrameLayout?>(bottomSheet!!).apply {
+                state = BottomSheetBehavior.STATE_EXPANDED
+                isHideable = true
+                skipCollapsed = true
+            }
+        }
+        dialog.dismissWithAnimation = true
+        return dialog
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layoutResId, container, false)
+    }
 }
--- moneymanager/feature_transaction_history/src/main/java/com/elli0tt/feature_transaction_history/presentation/add_transaction/AddTransactionBottomSheetFragment.kt
+++ moneymanager/feature_transaction_history/src/main/java/com/elli0tt/feature_transaction_history/presentation/add_transaction/AddTransactionBottomSheetFragment.kt
@@ -1,9 +1,7 @@
 package com.elli0tt.feature_transaction_history.presentation.add_transaction
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.elli0tt.feature_transaction_history.R
 import com.elli0tt.feature_transaction_history.databinding.FragmentAddTransactionBottomSheetBinding
@@ -13,7 +11,8 @@ import com.elli0tt.money_manager.base.extensions.viewBinding
 import com.elli0tt.money_manager.base.fragment.BaseBottomSheetDialogFragment
 import javax.inject.Inject
 
-class AddTransactionBottomSheetFragment : BaseBottomSheetDialogFragment() {
+class AddTransactionBottomSheetFragment :
+    BaseBottomSheetDialogFragment(R.layout.fragment_add_transaction_bottom_sheet) {
 
     companion object {
         const val TAG = "AddTransactionBottomSheetFragmentTag"
@@ -24,14 +23,6 @@ class AddTransactionBottomSheetFragment : BaseBottomSheetDialogFragment() {
     private lateinit var viewModel: AddTransactionBottomSheetViewModel
 
     private val binding by viewBinding(FragmentAddTransactionBottomSheetBinding::bind)
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_add_transaction_bottom_sheet, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
